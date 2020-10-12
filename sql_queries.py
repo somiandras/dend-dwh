adb0b42f0d827824fa336bcc660e2dfa27c1c59e
András Somi <somiandras@gmail.com>
2020-10-12 14:10:20 +0200

    Add insert queries

--- sql_queries.py
+++ sql_queries.py
@@ -116,32 +116,163 @@ time_table_create = """
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = (
+    """
+        copy staging_event from {bucket}
+        credentials 'aws_iam_role={arn}'
+        region 'us-west-2'
+        json {jsonpath};
+    """
+).format(
+    bucket=config.get("S3", "LOG_DATA"),
+    arn=config.get("IAM_ROLE", "ARN"),
+    jsonpath=config.get("S3", "LOG_JSONPATH"),
+)
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = (
+    """
+        copy staging_song from {bucket}
+        credentials 'aws_iam_role={arn}'
+        region 'us-west-2'
+        json 'auto';
+    """
+).format(bucket=config.get("S3", "SONG_DATA"), arn=config.get("IAM_ROLE", "ARN"))
 
 # FINAL TABLES
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = """
+    INSERT INTO songplay (
+        start_time,
+        user_id,
+        song_id,
+        artist_id,
+        session_id,
+        user_agent
+    )
+    SELECT
+        ts as start_time,
+        userId as user_id,
+        song_id,
+        artist_id,
+        sessionId as session_id,
+        userAgent as user_agent
+    FROM staging_event e
+    LEFT JOIN staging_song s ON
+        e.artist = s.artist_name AND
+        e.song = s.title AND
+        e.length = s.duration;
+"""
 
-user_table_insert = ("""
-""")
+user_table_insert = """
+    -- Get last songplay event for every userId in staging table
+    CREATE TEMP TABLE user_last_song AS
+    SELECT staging_event.*
+    FROM (
+        SELECT userId, max(ts) as ts
+        FROM staging_event
+        WHERE page = 'NextSong'
+        GROUP BY userId
+    ) last_ts
+    LEFT JOIN staging_event USING (userId, ts)
+    WHERE page = 'NextSong';
 
-song_table_insert = ("""
-""")
+    -- Insert rows for userIds that are not already in users table
+    INSERT INTO users
+    SELECT
+        uls.userId as user_id,
+        uls.firstName as first_name,
+        uls.lastName as last_name,
+        uls.gender,
+        uls.level
+    FROM user_last_song uls
+    LEFT JOIN users ON users.user_id = uls.userId
+    WHERE users.user_id IS NULL;
 
-artist_table_insert = ("""
-""")
+    -- Update existing users' level to latest value where it changed
+    UPDATE users
+    SET level = user_last_song.level
+    FROM user_last_song
+    WHERE
+        user_id = user_last_song.userId AND
+        users.level != user_last_song.level;
+"""
 
-time_table_insert = ("""
-""")
+song_table_insert = """
+    INSERT INTO song
+    SELECT
+        song_id,
+        staging_song.title,
+        staging_song.artist_id,
+        staging_song.year,
+        staging_song.duration
+    FROM staging_song
+    LEFT JOIN song USING (song_id)
+    WHERE
+        song.title IS NULL AND
+        song_id IN (SELECT DISTINCT song_id FROM songplay);
+"""
+
+artist_table_insert = """
+    INSERT INTO artist
+    SELECT
+        artist_id,
+        staging_song.artist_name AS name,
+        staging_song.artist_location AS location,
+        staging_song.artist_latitude AS latitude,
+        staging_song.artist_longitude AS longitude
+    FROM staging_song
+    LEFT JOIN artist USING (artist_id)
+    WHERE
+        artist.name IS NULL AND
+        artist_id IN (SELECT DISTINCT artist_id FROM songplay);
+"""
+
+time_table_insert = """
+    INSERT INTO time
+    WITH converted AS (
+        SELECT
+            ts,
+            'epoch'::timestamp + ts / 1000.0 * '1 second'::interval AS ts_converted
+        FROM staging_event
+    )
+    SELECT
+        ts AS start_time,
+        date_part('hour', ts_converted) AS hour,
+        date_part('day', ts_converted) AS day,
+        date_part('week', ts_converted) AS week,
+        date_part('month', ts_converted) AS month,
+        date_part('year', ts_converted) AS year,
+        date_part('dow', ts_converted) AS weekday
+    FROM converted
+    LEFT JOIN time ON time.start_time = converted.ts
+    WHERE time.start_time IS NULL;
+"""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    staging_events_table_create,
+    staging_songs_table_create,
+    time_table_create,
+    artist_table_create,
+    song_table_create,
+    user_table_create,
+    songplay_table_create,
+]
+drop_table_queries = [
+    staging_events_table_drop,
+    staging_songs_table_drop,
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop,
+]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+insert_table_queries = [
+    songplay_table_insert,
+    user_table_insert,
+    song_table_insert,
+    artist_table_insert,
+    time_table_insert,
+]
